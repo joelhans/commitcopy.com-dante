8537f231231a83957fb4c22fb012ce7706a0d38c
Joel Hans <j@joelhans.com>
2024-04-14 19:04:45 +0000

    Add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://commitcopy.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
+  site: 'https://commitcopy.com',
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: vercel()
 });
