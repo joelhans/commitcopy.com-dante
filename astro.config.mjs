9e140ad7516e884c52f8dc357e8d95b5d262cc5e
Joel Hans <j@joelhans.com>
2024-04-14 19:00:02 +0000

    Add Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://commitcopy.com',
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
 });
